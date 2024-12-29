53736d24b38f5732fb43f3e1325e4513324ebf4d
Laura LEROY <laura.leroy@edu.esiee.fr>
2024-12-29 20:36:55 +0100

    Data to use

--- src/utils/get_data.py
+++ src/utils/get_data.py
@@ -2,10 +2,15 @@ import os
 import requests
 import json
 
-response_API = requests.get('https://data.education.gouv.fr/api/explore/v2.1/catalog/datasets/fr-esr-parcoursup/records?limit=1')
+#Parcoursup : propositions d'admission dans l'enseignement supérieur des élèves de terminale diplômés du baccalauréat général selon leurs enseignements de spécialité (2021 à 2023)
+response_API_admiss = requests.get('https://data.enseignementsup-recherche.gouv.fr/api/explore/v2.1/catalog/datasets/fr-esr-parcoursup-enseignements-de-specialite-bacheliers-generaux-2/records?limit=20')
+#Cartographie des formation parcoursup (2021 à 2025 SANS 2024)
+response_carto_API = requests.get('https://data.enseignementsup-recherche.gouv.fr/explore/dataset/fr-esr-cartographie_formations_parcoursup/api/?disjunctive.tf&disjunctive.nm&disjunctive.fl&disjunctive.nmc&disjunctive.amg&sort=-annee&location=7,49.04147,9.33289&basemap=e69ab1')
+#Parcoursup 2022 - vœux de poursuite d'études et de réorientation dans l'enseignement supérieur et réponses des établissements
+response_API_2022 = requests.get('https://data.enseignementsup-recherche.gouv.fr/api/explore/v2.1/catalog/datasets/fr-esr-parcoursup_2022/records?limit=20')#juste l'année a changer !!!sauf 2023 (cest juste fr-esr-parcoursup)
 
-if response_API.status_code == 200:
-    data = response_API.text
+if response_API_2022.status_code == 200:
+    data = response_API_2022.text
     parsed_data = json.loads(data)
 
     output_dir = 'data/raw'
@@ -18,4 +23,4 @@ if response_API.status_code == 200:
     
     print(f"Les données ont été enregistrées dans {output_file}")
 else:
-    print("La requête API a échoué avec le code :", response_API.status_code)
+    print("La requête API a échoué avec le code :", response_API_2022.status_code)
