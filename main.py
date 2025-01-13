0d6af05eaf448886ce985254566e626da5238587
Harulla <laura.leroy@edu.esiee.fr>
2025-01-13 16:29:06 +0100

    Graphiques basé sur un établissement cliqué

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ import dash_leaflet as dl
 import dash_leaflet.express as dlx
 import json
 import requests
-# Ajouter les chemins requis
+import plotly.express as px
+import plotly.graph_objects as go
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'src', 'pages')))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'src', 'components')))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'src', 'utils')))
 
-from map import fetch_data_from_geojson
+from src.components.map import fetch_data_from_geojson
 
 # Initialiser l'application Dash
 app = Dash(__name__, suppress_callback_exceptions=True)
@@ -73,7 +75,7 @@ def update_geojson_data(annee_cible):
                 },
                 "properties": {
                     "etab_nom": feature["etab_nom"],
-                    "nom_formation": feature["nom_formation"],
+                    "formations": feature["formations"],
                     "etab_uai": feature["etab_uai"],
                 },
             }
@@ -83,40 +85,196 @@ def update_geojson_data(annee_cible):
     return geojson_data
 
 
+
+def process_api_response(response):
+
+    if(response["total_count"] == 0):
+        return []
+    
+    data = {
+        'total_count': response["results"][0],
+        'etab_uai': response["results"][0].get('cod_uai', ''),
+        'dep': response["results"][0].get('dep', ''),
+        'dep_lib': response["results"][0].get('dep_lib', ''),
+        'nom_etab': response["results"][0].get('g_ea_lib_vx', ''),
+        'academie': response["results"][0].get('acad_mies', ''),
+        'session': response["results"][0].get('session', ''),
+        'region': response["results"][0].get('region_etab_aff', ''),
+        'ville': response["results"][0].get('ville_etab', ''),
+    }
+    # Extraire la clé 'results' qui contient les formations
+    if 'results' in response:
+        results = []
+        for formation in response['results']:
+            # Création d'un dictionnaire pour chaque formation avec ses informations spécifiques
+            print(formation)
+            formation_data = {
+                'intitule_formation': formation.get('lib_for_voe_ins', ''),
+                'selectivite': formation.get('select_form', ''),
+                'capacite': formation.get('capa_fin', ''),
+                'effectif_total_candidat': formation.get('voe_tot', ''),
+                'effectif_total_candidat_femme': formation.get('voe_tot_f', ''),
+                'effectif_total_candidat_bg_pp': formation.get('nb_voe_pp_bg', ''),#bac général  PHASE PRINCIPALE
+                'effectif_total_candidat_bt_pp': formation.get('nb_voe_pp_bt', ''),#bac technologique
+                'effectif_total_candidat_bp_pp': formation.get('nb_voe_pp_bp', ''),#bac pro
+                'effectif_total_candidat_autre_pp': formation.get('nb_voe_pp_at', ''), #autre formation
+                'proposition_admission_total_BG': formation.get('prop_tot_bg', ''),#PROPOSITION D'ADMISSION
+                'proposition_admission_total_BT': formation.get('prop_tot_bt', ''),
+                'proposition_admission_total_BP': formation.get('prop_tot_bp', ''),
+                'proposition_admission_total_autre': formation.get('prop_tot_at', ''),
+                'acceptation_total': formation.get('acc_tot', ''), #effectif des candidat ayant accepté la proposition d'admission
+                'acceptation_total_f': formation.get('acc_tot_f', ''),#dont femme
+                'effectif_admis_BG': formation.get('acc_bg', ''), #effectif admis bag général ectc...
+                'effectif_admis_BT': formation.get('acc_bt', ''),
+                'effectif_admis_BP': formation.get('acc_bp', ''),
+                'effectif_admis_at': formation.get('acc_at', ''),
+                'effectif_admis_mention_inconnue': formation.get('acc_mention_nonrenseignee', ''),
+                'effectif_admis_mention_aucune': formation.get('acc_sansmention', ''),
+                'effectif_admis_mention_AB': formation.get('acc_ab', ''),
+                'effectif_admis_mention_B': formation.get('acc_b', ''),
+                'effectif_admis_mention_TB': formation.get('acc_tb', ''),
+                'effectif_admis_mention_TBF': formation.get('acc_tbf', ''),
+                'rang_dernier_admis': formation.get('ran_grp1', ''),
+                'lien_form_psup': formation.get('lien_form_psup', ''),
+            }
+            results.append(formation_data)
+
+        # Ajouter la liste des formations dans le dictionnaire des données
+        data['results'] = results
+    
+    return data
+
+def generate_pie_chart(result):
+    labels = ['Sans Mention', 'Mention AB', 'Mention B', 'Mention TB', 'Mention TBF']
+    values = [
+        result['effectif_admis_mention_aucune'],
+        result['effectif_admis_mention_AB'],
+        result['effectif_admis_mention_B'],
+        result['effectif_admis_mention_TB'],
+        result['effectif_admis_mention_TBF'],
+    ]
+    fig = px.pie(
+        names=labels,
+        values=values,
+        title=f"Répartition des mentions au bac des admis pour la formation {result['intitule_formation']}"
+    )
+    return fig
+
+def create_nested_pie_chart(data):
+    # Extraction des données de base
+    candidats = [
+        data['effectif_total_candidat_bg_pp'],
+        data['effectif_total_candidat_bt_pp'],
+        data['effectif_total_candidat_bp_pp'],
+        data['effectif_total_candidat_autre_pp'],
+    ]
+    propositions = [
+        data['proposition_admission_total_BG'],
+        data['proposition_admission_total_BT'],
+        data['proposition_admission_total_BP'],
+        data['proposition_admission_total_autre'],
+    ]
+    admis = [
+        data['effectif_admis_BG'],
+        data['effectif_admis_BT'],
+        data['effectif_admis_BP'],
+        data['effectif_admis_at'],
+    ]
+
+    # Calcul des catégories opposées proposition vs refusés, admis vs voeux non accepté
+    refuses = [max(c - p, 0) for c, p in zip(candidats, propositions)]  # Refusé
+    voeux_non_acceptes = [max(p - a, 0) for p, a in zip(propositions, admis)]  # Vœux non acceptés
+
+    labels = [
+        "Candidats BG", "Propositions BG", "Admis BG", "Vœux non acceptés BG", "Refusé BG",
+        "Candidats BT", "Propositions BT", "Admis BT", "Vœux non acceptés BT", "Refusé BT",
+        "Candidats BP", "Propositions BP", "Admis BP", "Vœux non acceptés BP", "Refusé BP",
+        "Candidats Autres", "Propositions Autres", "Admis Autres", "Vœux non acceptés Autres", "Refusé Autres",
+    ]
+
+    # Définition des parents pour la hiérarchie
+    parents = [
+        "", "Candidats BG", "Propositions BG", "Propositions BG", "Candidats BG",  # BG
+        "", "Candidats BT", "Propositions BT", "Propositions BT", "Candidats BT",  # BT
+        "", "Candidats BP", "Propositions BP", "Propositions BP", "Candidats BP",  # BP
+        "", "Candidats Autres", "Propositions Autres", "Propositions Autres", "Candidats Autres",  # Autres
+    ]
+
+    values = [
+        candidats[0], propositions[0], admis[0], voeux_non_acceptes[0], refuses[0],  # BG
+        candidats[1], propositions[1], admis[1], voeux_non_acceptes[1], refuses[1],  # BT
+        candidats[2], propositions[2], admis[2], voeux_non_acceptes[2], refuses[2],  # BP
+        candidats[3], propositions[3], admis[3], voeux_non_acceptes[3], refuses[3],  # Autres
+    ]
+
+    colors = [
+        "#1B4965", "#0081A7", "#00AFB9", "#62B6CB", "#BEE9E8",  # BG  --> Candidats, Propositions, Admis, Vœux non acceptés, Refusé, 
+        "#FF7B00", "#FF9500", "#FFA200", "#FFB700", "#FF8800",  # BT
+        "#55753C", "#96BE8C", "#ACECA1", "#C9F2C7", "#629460",  # BP
+        "#6E0D0D", "#F73E3E", "#A81111", "#FF7777", "#DE1021",  # Autres
+    ]
+
+    fig = go.Figure(go.Sunburst(
+        labels=labels,
+        parents=parents,
+        values=values,
+        branchvalues="total",  # Les parents incluent les enfants
+        marker=dict(colors=colors)
+    ))
+
+    # Mise en forme de la figure
+    fig.update_layout(
+        title="Répartition des candidats, propositions et admis par type de bac",
+        margin=dict(t=40, l=0, r=0, b=0),
+    )
+
+    return fig
+
+
 # Callback pour gérer les clics sur les clusters ou marqueurs individuels
 @app.callback(
     Output("api-result-container", "children"),
-    Input("geojson-layer", "clickData"),  # Détecte les clics sur une feature GeoJSON
+    Input("geojson-layer", "clickData"),
     prevent_initial_call=True,
 )
 def fetch_api_data(feature):
     if not feature:
         return "Cliquez sur un marqueur pour voir les informations."
-    print(feature)
 
     properties = feature.get("properties", {})
     cod_uai = properties.get("etab_uai", None)
-    print(cod_uai)
+
     if not cod_uai:
         return "Aucun code UAI trouvé pour ce marqueur."
 
     api_url = f"https://data.enseignementsup-recherche.gouv.fr/api/explore/v2.1/catalog/datasets/fr-esr-parcoursup/records?where=cod_uai%20LIKE%20%22{cod_uai}%22"
     response = requests.get(api_url)
-    data = response.json()
-    print("results")
-    print("----------------")
-    if "results" in data and len(data["results"]) > 0:
-        info = data["results"][0] # A CHANGER CAR PEUT AVOIR PLUSIEURS FORMATION POUR UN MEME ETABLISSEMENT (EX IUT MARNE LA VALLEE)
-        print(info)
-        return html.Div([
-            html.H3(f"Détails de l'établissement {info['lib_for_voe_ins']}"),
-            html.P(f"Région: {info['region_etab_aff']}"),
-            html.P(f"Ville: {info['ville_etab']}"),
-            html.P(f"Formation Sélective: {info['select_form']}"),
-            html.A("Voir la fiche", href=info["detail_forma"], target="_blank"),
-        ])
-    else:
+    data = process_api_response(response.json())
+
+    if len(data) == 0:
         return html.P("Aucune information disponible pour cet établissement.")
 
+    divs = [
+        html.H3(f"Détails de l'établissement {data['nom_etab']}"),
+        html.P(f"Session: {data['session']}"),
+        html.P(f"Académie: {data['academie']}"),
+        html.P(f"Ville: {data['ville']}"),
+        html.P(f"Département: {data['dep_lib'] + '-' + data['dep']}"),
+        html.P(f"Région: {data['region']}"),
+    ]
+
+    # Ajouter les détails pour chaque formation
+    if "results" in data and len(data["results"]) > 0:
+        for result in data["results"]:
+            divs.append(html.Div([
+                html.P(f"Intitulé de la formation: {result['intitule_formation']}"),
+                html.P(f"Formation Sélective: {result['selectivite']}"),
+                html.A("Voir la fiche", href=result["lien_form_psup"], target="_blank"),
+                dcc.Graph(figure=generate_pie_chart(result), style={'display': 'inline-block', 'width': '49%'}),  # graphique des mentions
+                dcc.Graph(figure=create_nested_pie_chart(result), style={'display': 'inline-block', 'width': '49%'}) #graphiques avec type de bac
+            ]))
+        return html.Div(divs)
+    else:
+        return html.P("Aucune information disponible pour cet établissement.")
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=False)
--- src/components/map.py
+++ src/components/map.py
@@ -5,29 +5,45 @@ import folium
 from folium.plugins import MarkerCluster
 
 def fetch_data_from_geojson(file_path, year):
-    """ Récupère les données du GeoJSON pour une année donnée """
+    """ Récupère les données du GeoJSON pour une année donnée, en regroupant les formations par établissement """
     print(f"Chargement des données depuis le fichier GeoJSON : {file_path}...")
 
     with open(file_path, 'r', encoding='utf-8') as file:
         geojson_data = json.load(file)
 
-    features = []
+    establishments = {}
+
     for feature in geojson_data['features']:
         properties = feature['properties']
         geometry = feature['geometry']
 
         if properties['annee'] == year:
-            features.append({
-                'etab_nom': properties['etab_nom'],
-                'etab_uai': properties['etab_uai'],
-                'annee': properties['annee'],
-                'nom_formation': properties['nm'],
-                'latitude': geometry['coordinates'][1],
-                'longitude': geometry['coordinates'][0]
-            })
+            etab_uai = properties['etab_uai']
+            formation = properties['nm'][0]  # Assuming that 'nm' is a list and you take the first formation
 
-    print(f"{len(features)} établissements trouvés pour l'année {year}.")
-    return features
+            if etab_uai not in establishments:
+                establishments[etab_uai] = {
+                    'etab_nom': properties['etab_nom'],
+                    'etab_uai': etab_uai,
+                    'annee': properties['annee'],
+                    'tc': properties['tc'],
+                    'region': properties['region'],
+                    'departement': properties['departement'],
+                    'commune': properties['commune'],
+                    'fiche': properties['fiche'],
+                    'etab_url': properties['etab_url'],
+                    'latitude': geometry['coordinates'][1],
+                    'longitude': geometry['coordinates'][0],
+                    'formations': []  # Initialize an empty list for formations
+                }
+            
+            # Ajouter la formation à la liste des formations de l'établissement
+            establishments[etab_uai]['formations'].append(formation)
+
+    # Convertir le dictionnaire en une liste de résultats
+    results = list(establishments.values())
+    print(f"{len(results)} établissements trouvés pour l'année {year}.")
+    return results
 
 
 
